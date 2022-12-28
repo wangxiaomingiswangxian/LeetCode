812ef400802ac5d6ecdedeb3376713884c07008b
wangxiaoming <1940694270@qq.com>
2022-12-28 14:49:20 +0800

    Kmp算法求解

--- src/main/java/com/alorithm/easy/MaxRepeatSubString.java
+++ src/main/java/com/alorithm/easy/MaxRepeatSubString.java
@@ -10,21 +10,21 @@ public class MaxRepeatSubString {
     /**
      * 给你一个字符串sequence，如果字符串 word连续重复k次形成的字符串是sequence的一个子字符串，那么单词word 的 重复值为 k 。
      * 单词 word的 最大重复值是单词word在sequence中最大的重复值。如果word不是sequence的子串，那么重复值k为 0 。
-     *
+     * <p>
      * 给你一个字符串 sequence和 word，请你返回 最大重复值k 。
-     *
+     * <p>
      * 来源：力扣（LeetCode）
      * 链接：https://leetcode.cn/problems/maximum-repeating-substring
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
     public static void main(String[] args) {
-        System.out.println(maxRepeating("ababc",
+        System.out.println(maxRepeatingKmp("ababc",
                 "ab"));
-        System.out.println(maxRepeating("ababc",
+        System.out.println(maxRepeatingKmp("ababc",
                 "ba"));
-        System.out.println(maxRepeating("ababc",
+        System.out.println(maxRepeatingKmp("ababc",
                 "ac"));
-        System.out.println(maxRepeating("aaabaaaabaaaabaaaabaaaaba",
+        System.out.println(maxRepeatingKmp("aaabaaaabaaaabaaaabaaaaba",
                 "aaaba"));
     }
 
@@ -50,5 +50,55 @@ public class MaxRepeatSubString {
         }
 
         return Arrays.stream(f).max().getAsInt();
+    }
+
+    /**
+     * kmp算法
+     */
+    public static int maxRepeatingKmp(String sequence, String word) {
+        int n = sequence.length(), m = word.length();
+        if (n < m) {
+            return 0;
+        }
+        int[] f = new int[n];
+        int[] next = getPresuffix(word);
+
+        int j = -1;
+        for (int i = 0; i < n; i++) {
+            while (j != -1 && word.charAt(j + 1) != sequence.charAt(i)) {
+                j = next[j];
+            }
+            if (word.charAt(j + 1) == sequence.charAt(i)) {
+                ++j;
+                if (j == m - 1) {
+                    f[i] = (i > m ? f[i - m] : 0) + 1;
+                    j = next[j];
+                }
+            }
+        }
+
+
+        return Arrays.stream(f).max().getAsInt();
+    }
+
+    /**
+     * 本质上是为了解决j回溯到起点的问题
+     * 为何命名
+     */
+    @SuppressWarnings("unused")
+    public static int[] getPresuffix(String sequence) {
+        final int length = sequence.length();
+        int[] next = new int[length];
+        Arrays.fill(next, -1);
+        for (int i = 1; i < length; i++) {
+            int j = next[i - 1];
+            while (j != -1 && sequence.charAt(j + 1) != sequence.charAt(i)) {
+                j = next[j];
+            }
+            if (sequence.charAt(j + 1) == sequence.charAt(i)) {
+                next[i] = j + 1;
+            }
+        }
+        return next;
     }
 }
