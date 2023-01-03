8819c8a5e0298efcb17bff56e7fa84081d4b4361
wangxiaoming <1940694270@qq.com>
2023-01-03 09:49:11 +0800

    三数之和

---
+++ src/main/java/com/alorithm/normal/ThreeSum.java
@@ -0,0 +1,60 @@
+package com.alorithm.normal;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author 王贤
+ */
+public class ThreeSum {
+    public static void main(String[] args) {
+        final ThreeSum threeSum = new ThreeSum();
+        int[] nums ={-1,0,1,2,-1,-4};
+        System.out.println(threeSum.threeSum(nums));
+    }
+
+    /**
+     * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+     *
+     * 你返回所有和为 0 且不重复的三元组。
+     *
+     * 注意：答案中不可以包含重复的三元组。
+
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/3sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int first = 0; first < n; first++) {
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            int third = n - 1;
+            int target = -nums[first];
+            for (int second = first + 1; second < n; second++) {
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                while (second < third && nums[second] + nums[third] > target) {
+                    third--;
+                }
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    res.add(list);
+                }
+            }
+        }
+
+        return res;
+    }
+}
