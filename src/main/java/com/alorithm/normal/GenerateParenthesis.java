3e6952444adef3a9db2967d5ffacb4d9a60e1d78
wangxiaoming <1940694270@qq.com>
2023-01-22 18:31:33 +0800

    生成括号

---
+++ src/main/java/com/alorithm/normal/GenerateParenthesis.java
@@ -0,0 +1,24 @@
+package com.alorithm.normal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 王贤
+ */
+public class GenerateParenthesis {
+    public static void main(String[] args) {
+
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+
+        return res;
+    }
+
+    private String create(String s){
+
+    return "("+s+")";
+    }
+}
